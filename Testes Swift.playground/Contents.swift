154d705f40e80f1691cc290270132d7f43222501
icaro rangel <icarogin2@gmail.com>
2023-09-16 21:45:58 -0400

    Adicionando questões
    questao de dictionarie e alguns comentarios

--- Testes Swift.playground/Contents.swift
+++ Testes Swift.playground/Contents.swift
@@ -1,20 +1,66 @@
 import UIKit
 
-// MARK: - Teste de Dictionarie
-//OBS: comente aquele que for o errado e a opcao que for a certa coloca um comentario de o porque a
+//MARK: - Teste de Dictionarie
+//OBS: comente aquele que for o errado e a Opção que for a certa coloca um comentario e diga o porque dela ser a certa
 //Qual dessas linhas cria Dictionarie?
-//opcao 1:
+
+//Questão 1:
+
+//Opção 1:
 var roles = ["captain": "Mal", "engineer": "Kaylee"]
-//opcao 2:
+//Opção 2:
 var place = ["road", "Park Lane", "city", "Cardiff"]
 
-//Resposta: op 1 por que Isso cria um dicionário com strings para chaves e strings para valores.
-//A op 2 cria um Array
+//Resposta: Opção 1 por que Isso cria um Dictionarie com strings para chaves e strings para valores.
+//A Opção 2 cria um Array
 
-//opcao 1:
+
+//Questão 2:
+//Opção 1:
 //let location = ("road": "Park Lane", "city": "Cardiff")
-//opcao 2:
+//Opção 2:
 let Heights = ["Taylor Swift": 1.79]
 
-//Resposta: op 1 por que Isso cria um dictionarie com strings para keys e Doubles para valores.
-//A op 1 não é um código Swift válido.
+//Resposta: Opção 1 por que Isso cria um Dictionarie com strings para keys e Doubles para valores.
+//A Opção 1 não é um código Swift válido entao ele vai dar erro por isso esta comentado.
+
+
+//Questão 3:
+//Opção 1:
+var roles = ["captain": "Mal", "engineer": "Kaylee"]
+//Opção 2:
+var place = ["road", "Park Lane", "city", "Cardiff"]
+//Resposta: Opção 1 por que isso cria um Dictionarie com strings para chaves e strings para valores.
+//A Opção 2 cria um Array e nao um Dictionarie.
+
+
+//Questão 4:
+//Opção 1:
+var speed = 60.75
+//Opção 2:
+var capitals = ["England": "London", "Scotland": "Edinburgh"]
+//Resposta: Opção 2 cria um Dictionarie com strings para chaves e strings para valores.
+//A Opção 1 cria um Double e nao um Dictionarie.
+
+
+//Questão 5:
+//Opção 1:
+let forecast = ["Monday": "sunny", "Tuesday": "cloudy"]
+//Opção 2:
+let isVisible = true
+//Resposta: Opção 1 cria um Dictionarie com strings para chaves e strings para valores.
+//A Opção 1 cria um Booleano e nao um Dictionarie.
+
+
+
+//Questão 6:
+//Opção 1:
+let scores = ["Sophie": 100]
+//Opção 2:
+let password = "fr0sti3s"
+//Resposta: Opção 1 cria um Dictionarie com strings para chaves e inteiros para valores.
+//A Opção 2 cria uma String e nao um Dictionarie.
+
+//Nota: Revise e pratique bastante, crie seus proprios codigos e nao perca o foco.
+
+
